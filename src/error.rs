70a73713c1b6dab476921ed07e09526327b169d3
4JX <79868816+4JX@users.noreply.github.com>
2021-12-19 22:00:22 +0100

    Initial commit

---
+++ src/error.rs
@@ -0,0 +1,11 @@
+use hidapi::HidError;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+#[allow(clippy::enum_variant_names)]
+pub enum Error {
+    #[error("HidError: {}",.0)]
+    HidError(#[from] HidError),
+    #[error("Error: Couldn't find device")]
+    DeviceNotFound,
+}
